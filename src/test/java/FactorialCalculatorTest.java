1a4b9ad1b040ca41b34b21c24ac2e8ce5b7918c3
FabulousDonkey777 <oceaniagb1984@gmail.com>
2025-02-19 22:10:52 +0300

    Lesson_13_testng

---
+++ src/test/java/FactorialCalculatorTest.java
@@ -0,0 +1,22 @@
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class FactorialCalculatorTest {
+
+    @Test
+    public void testFactorialOfZero() {
+        assertEquals(1, FactorialCalculator.calculateFactorial(0))     ;
+    }
+
+    @Test
+    public void testFactorialOfPositiveNumber() {
+        assertEquals(120, FactorialCalculator.calculateFactorial(5));
+        assertEquals(720, FactorialCalculator.calculateFactorial(6));
+    }
+
+    @Test(expectedExceptions = IllegalArgumentException.class, expectedExceptionsMessageRegExp = "Факториал отрицательного числа не существует.")
+    public void testFactorialOfNegativeNumber() {
+        FactorialCalculator.calculateFactorial(-1);
+    }
+}
